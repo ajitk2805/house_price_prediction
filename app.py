05490d4f04d1c3ff480651fd6206290dc439a7a4
Ajit <ajitk2805@gmail.com>
2023-06-23 15:55:14 +0530

    components

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+import sys, os
+from housing.logger import logging
+from housing.exception import HousingException
 
 application = Flask(__name__)
 app = application
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    try:
+        raise Exception('Testing custom exception')
+        # logging.info('In app.py')
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info('Testing logging module')
     return "Welcome to Machine Learning Project"
 
 if __name__=='__main__':
---
+++ housing/logger/__init__.py
@@ -0,0 +1,13 @@
+import logging, os
+from datetime import datetime
+
+LOG_DIR = "housing_logs"
+CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+LOG_FILE_NAME = F"log_{CURRENT_TIME_STAMP}.log"
+
+os.makedirs(LOG_DIR, exist_ok=True)
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE_NAME)
+
+logging.basicConfig(filename=LOG_FILE_PATH, filemode='w', 
+                    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+                    level=logging.INFO)
